5e1b136a4c8ddf24107c3403ed6b55c5e91d5eef
John Farrell <jsfarrell12@gmail.com>
2022-12-13 11:32:03 -0500

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
+      <Routes basename={`https://admin.xctf.live`}>
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/" element={<Dashboard />} />
       </Routes>
